fbd895cf08a1874d87c5a5aad8343d9d559651a7
Samb87e <Samsonbakare99@gmail.com>
2025-08-12 23:34:59 +0100

    Add files via upload

---
+++ pi-wallet-cli-main/src/makePayment.js
@@ -0,0 +1,121 @@
+const chalk = require('chalk')
+const Stellar = require('stellar-sdk')
+var StellarBase = require('stellar-base');
+const config = require('../config.json');
+const piLib = require('./piLib');
+const prompt = require('prompt-sync')({ sigint: true });
+const CLI = require('clui');
+const Spinner = CLI.Spinner;
+
+function makePayment() {
+
+    piLib.createBanner('Make Payment');
+
+    //get source account information
+    const accountAddress = (config.my_address) ? config.my_address : prompt(chalk.yellowBright('Source Account Address: '));
+    const accountPassphrase = prompt(chalk.yellowBright('Source Account Passphrase/PrivateKey: '));
+    
+    //ask confirmation
+    prompt(chalk.yellowBright('Press Enter to Finalize and Submit...'));
+
+    const status = new Spinner('Making transaction, please wait...');
+    status.start();
+
+    //create server object
+    const server = new Stellar.Server(config.server)
+
+    const getKeyPair = (StellarBase.StrKey.isValidEd25519SecretSeed(accountPassphrase)) ? piLib.getKeyPairFromSecret : piLib.getKeyPairFromPassphrase;
+
+    const fail = (message) => {
+        console.log('\n')
+        console.error(chalk.red(message))
+        if (message.response && message.response.data && message.response.data.extras && message.response.data.extras.result_codes && message.response.data.extras.result_codes.operations) {
+            const reason = message.response.data.extras.result_codes.operations;
+            switch(reason) {
+                case 'op_underfunded':
+                    console.log(chalk.red('reason:', 'Sender account has insufficient funds'));
+                    break;
+                default:
+                    console.log(chalk.red('reason:', reason))
+            }
+        }
+        process.exit(1)
+    }
+
+    const success = (tn) => {
+        status.stop();
+        if (tn.successful){
+            console.log(chalk.magentaBright(`\nTransaction succeeded!\nLink: ${tn._links.transaction.href}`))
+        }else{
+            console.log(chalk.red('\nTransaction Failed'))
+        }
+    }
+
+    // New loop to handle multiple payments
+    const makeMultiplePayments = async () => {
+        let continueSending = true;
+        const keypair = await getKeyPair(accountPassphrase);
+
+        while (continueSending) {
+            //get destination account information
+            const destAccountAddress = prompt(chalk.yellowBright('Destination Account Address: '));
+            //get asset to transfer
+            const assetName = prompt(chalk.yellowBright('Asset (blank for native): '));
+            const issuerAddress = prompt(chalk.yellowBright('Asset Issuer (blank for native): '));
+            //get amount to transfer
+            const transferAmt = prompt(chalk.yellowBright('Transfer Amt: '));
+            //get memo to transfer
+            const transferMemo = prompt(chalk.yellowBright('Memo (optional): '));
+
+            //validate
+            if (!StellarBase.StrKey.isValidEd22519PublicKey(destAccountAddress)) {
+                console.log(chalk.red('Not a valid destination address'))
+                continue;
+            }
+            var transferAsset;
+            if(assetName && issuerAddress) {
+                transferAsset = new Stellar.Asset(assetName, issuerAddress);
+            }else if ((assetName && !issuerAddress) || (!assetName && issuerAddress)) {
+                throw "For sending assets, both asset name and issuer address must be set!"
+            }else{
+                transferAsset = Stellar.Asset.native();
+            }
+
+            const paymentToDest = {
+                destination: destAccountAddress,
+                asset: transferAsset,
+                amount: transferAmt,
+            }
+            const txOptions = {
+                fee: await server.fetchBaseFee(),
+                networkPassphrase: config.networkPassphrase,
+            }
+
+            try {
+                const accountA = await server.loadAccount(accountAddress);
+                const transaction = new Stellar.TransactionBuilder(accountA, txOptions)
+                    .addOperation(Stellar.Operation.payment(paymentToDest))
+                    .addMemo(Stellar.Memo.text(transferMemo))
+                    .setTimeout(StellarBase.TimeoutInfinite)
+                    .build();
+
+                transaction.sign(keypair);
+                const response = await server.submitTransaction(transaction);
+                success(response);
+
+                const again = prompt(chalk.yellowBright('Send another payment? (y/n): '));
+                if (again.toLowerCase() !== 'y') {
+                    continueSending = false;
+                }
+
+            } catch (error) {
+                fail(error);
+                continueSending = false; // Exit on a failure
+            }
+        }
+    }
+    
+    makeMultiplePayments().catch(fail);
+}
+
+module.exports = makePayment;
